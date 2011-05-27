a23185c856bd9fe118c6804a5917ac434f4b14e0
Christian Pulvermacher <christian@christian-laptop.(none)>
2011-05-28 00:22:18 +0200

    add bug tracker

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -91,7 +91,7 @@ MainWindow::MainWindow(QWidget *parent) :
 void MainWindow::about()
 {
 	QMessageBox::about(this, tr("About EvilAlarm"),
-		tr("<center><h1>EvilAlarm 0.8</h1>\
+		tr("<center><h1>EvilAlarm 0.9</h1>\
 An alarm clock which cannot be turned off while asleep\
 <small><p>&copy;2010, 2011 Christian Pulvermacher &lt;pulvermacher@gmx.de&gt;</p></small></center>\
 <p>This program is free software; License: <a href=\"http://www.gnu.org/licenses/gpl-2.0.html\">GNU GPL 2</a> or later.</p>"));
