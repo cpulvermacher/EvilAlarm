23eb8701267a8222f3b50076fd1e9b188819f995
christian <pulvermacher@gmx.de>
2011-08-22 19:31:34 +0200

    remove unused code & preprocessor statements

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -20,11 +20,6 @@
 #include <iostream>
 
 
-//#if defined(Q_WS_MAEMO)
-//#include <alarmd/alarm_event.h>
-//#endif
-
-
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -78,7 +73,6 @@ void MainWindow::showAlarmTypeSelector() {
 
 
 void MainWindow::setEvilAlarm(int hours, int minutes) {
-#ifdef EVILALARM
     std::cout << "setEvilAlarm()\n";
     QSettings settings;
 
@@ -107,11 +101,9 @@ void MainWindow::setEvilAlarm(int hours, int minutes) {
     settings.sync();
 
     Daemon::start();
-#endif
 }
 
 void MainWindow::unsetEvilAlarm() {
-#ifdef EVILALARM
     std::cout << "unsetEvilAlarm()\n";
     Daemon::stop();
 
@@ -130,47 +122,8 @@ void MainWindow::unsetEvilAlarm() {
     }
     settings.endGroup();
     settings.sync();
-#endif
 }
 
-/*void MainWindow::setAlarm(int hours,int minutes){
-    // from http://www.tardigrada.hr/blog/2010/10/using-maemos-alarm-framework-with-qt-the-basics/
-
-
-        alarm_event_t * event = 0;
-        alarm_action_t * act = 0;
-
-        // Setup
-        event = alarm_event_create();
-        // The application id field is needed for every alarm you set
-        alarm_event_set_alarm_appid(event, "Alarm testing app");
-        // Alarm description displayed when the alarm is triggered
-        alarm_event_set_message(event, "The alarm message for the system UI");
-        // Set the alarm time 30s form now
-        event->alarm_time = time(0) + 30;
-
-        // Acknowledge action
-        act = alarm_event_add_actions(event, 1);
-        alarm_action_set_label(act, "Stop");
-        act->flags |= ALARM_ACTION_WHEN_RESPONDED;
-        act->flags |= ALARM_ACTION_TYPE_NOP;
-
-        // Snooze action
-        act = alarm_event_add_actions(event, 1);
-        alarm_action_set_label(act, "Snooze");
-        act->flags |= ALARM_ACTION_WHEN_RESPONDED;
-        act->flags |= ALARM_ACTION_TYPE_SNOOZE;
-
-        // Pass the structure to the alarmd daemon
-        alarmd_event_add(event);
-        // Cleanup
-        alarm_event_delete(event);
-        act = 0;
-        event = 0;
-
-
-}
-*/
 void MainWindow::on_actionSettings_triggered()
 {
     Settings *settingsWindow = new Settings(this);
